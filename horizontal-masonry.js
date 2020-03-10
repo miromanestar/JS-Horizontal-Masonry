e5d91aad0e9dac1c2df2fc6e058fb23079ab7421
Miro Manestar <miroimanestar@gmail.com>
2020-03-09 21:42:17 -0400

    Update horizontal-masonry.js

--- horizontal-masonry.js
+++ horizontal-masonry.js
@@ -1,1 +1,13 @@
+(function() {
+  createElements();
+})();
 
+function createElements() {
+  for(let i = 0; i < 10; i++) {
+    var masonryDiv = document.createElement("div");
+    masonryDiv.setAttribute("class", "masonry-div");
+    masonryDiv.setAttribute("id", "masonry-div-" + i);
+    masonryDiv.innerText = i;
+    document.getElementById("masonry").appendChild(masonryDiv);
+  }
+}
